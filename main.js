fec91bde5c13c458406ba03127e7626b201bfdfd
NEWXY-WINS <newxygames@gmail.com>
2024-05-29 13:06:37 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+// script.js
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName("mySlides");
+    const dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    // Hide all slides
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    // Set active slide
+    slides[slideIndex - 1].style.display = "block";
+
+    // Update dot indicators
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+}
